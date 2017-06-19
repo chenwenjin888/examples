fe33e7761409dd5a06d5a59e4f96d5aa97292b3e
chenwenjin888 <253452632@qq.com>
2017-06-19 12:29:19 +0800

    增加案列展示首页模块

---
+++ resource/js/config.js
@@ -0,0 +1,48 @@
+/**
+ * config.js
+ * Author chen.wj
+ * Create: 2017/6/16
+ */
+var imgPath = 'resource/img/'
+var GConfig = [
+    {
+        imgSrc: imgPath+'01.png',
+        title: '瀑布流布局展示'
+    },
+    {
+        imgSrc: imgPath+'02.jpg',
+        title: '瀑布流布局展示'
+    },
+    {
+        imgSrc: imgPath+'03.png',
+        title: '瀑布流布局展示'
+    },
+    {
+        imgSrc: imgPath+'04.png',
+        title: '瀑布流布局展示'
+    },
+    {
+        imgSrc: imgPath+'01.png',
+        title: '瀑布流布局展示'
+    },
+    {
+        imgSrc: imgPath+'01.png',
+        title: '瀑布流布局展示'
+    },
+    {
+        imgSrc: imgPath+'02.jpg',
+        title: '瀑布流布局展示'
+    },
+    {
+        imgSrc: imgPath+'03.png',
+        title: '瀑布流布局展示'
+    },
+    {
+        imgSrc: imgPath+'04.png',
+        title: '瀑布流布局展示'
+    },
+    {
+        imgSrc: imgPath+'01.png',
+        title: '瀑布流布局展示'
+    },
+]
